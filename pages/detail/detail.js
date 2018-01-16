9a8873427f1b6da92877fc5d2c91142c0e9f4586
项金 <admxj@126.com>
2018-01-16 15:01:00 +0800

    项目提交

---
+++ pages/detail/detail.js
@@ -0,0 +1,328 @@
+
+var Api = require('../../utils/api.js');
+var util = require('../../utils/util.js');
+var WxParse = require('../../wxParse/wxParse.js');
+var app = getApp()
+
+Page({
+  data: {
+    title: '文章内容',
+    detail: {},
+    commentsList:{},
+    commentCount:'',
+    detailDate:'',
+   
+    wxParseData:[],
+    display:'none',
+    page: 1,
+    isLastPage:false,
+
+    postID:null,
+    scrollHeight: 0,
+
+    isGetUserInfo:false,
+
+    
+
+    dialog: {
+      title: '',
+      content: '',
+      hidden: true
+    },
+    content:'',
+    userInfo:[]
+  },
+  onLoad: function (options) {
+    this.fetchDetailData(options.id);
+    var self = this;
+    wx.getSystemInfo({
+      
+      success: function (res) {
+        //console.info(res.windowHeight);
+        self.setData({
+          scrollHeight: res.windowHeight,
+          
+        });
+      }
+    });
+
+    //获取用户信息
+    app.getUserInfo(function (userInfo) {
+      //更新数据
+      self.setData({
+        userInfo: userInfo,
+        isGetUserInfo:true
+      })
+    });
+  },
+  onShareAppMessage: function () {
+    return {
+      title: '分享文章：' + this.data.detail.title.rendered,
+      path: 'pages/detail/detail?id=' + this.data.detail.id,
+      success: function (res) {
+        // 转发成功
+      },
+      fail: function (res) {
+        // 转发失败
+      }
+    }
+  },
+  //获取文章内容
+  fetchDetailData: function (id) {
+    var self = this;
+   
+    wx.request({
+      url: Api.getPostByID(id, { mdrender: false }),
+      success: function (response) {
+        //console.log(response);
+        self.setData({
+          detail: response.data,
+          postID: id,
+          detailDate: util.cutstr(response.data.date, 10, 1),
+          //wxParseData: WxParse('md',response.data.content.rendered)
+          wxParseData: WxParse.wxParse('article', 'html', response.data.content.rendered, self, 5),
+          display: 'block'
+
+        });
+
+        self.fetchCommentData(self.data);       
+      }
+    });
+  },
+  //获取评论
+  fetchCommentData: function (data) {
+    var self = this;
+
+    if (!data) data = {};
+    if (!data.page) data.page = 1;
+
+    if (data.page === 1) {
+      self.setData({
+        commentsList: []
+      });
+    };
+    wx.request({
+      url: Api.getComments(data),
+      success: function (response) {
+        if (response.data.length < 6) {
+          self.setData({
+            isLastPage: true
+          });
+        }
+        self.data.commentsList;
+        self.setData({
+          //commentsList: response.data,
+         
+          commentsList: self.data.commentsList.concat(response.data.map(function (item) {
+            var strSummary = util.removeHTML(item.content.rendered);
+            var strdate = item.date
+            item.summary = strSummary;
+            item.date = util.formatDateTime(strdate);
+             if (item.author_url.indexOf('wx.qlogo.cn') !=-1 )
+            {
+              if (item.author_url.indexOf('https') ==-1 )
+              {
+                item.author_url = item.author_url.replace("http", "https");
+              }
+
+              
+            }
+            else
+            {
+              item.author_url ="../../images/gravatar.png";
+            }
+            
+            return item;
+           
+          })),
+          commentCount: "有" + response.data.length + "条评论",
+          
+        });
+
+
+        wx.showToast({
+          title: '加载中',
+          icon: 'loading',
+          mask: false,
+          duration: 1000         
+
+        })
+        
+      }
+    });
+  },
+  //底部刷新
+  loadMore: function (e) {
+
+    var self = this;
+    if (!self.data.isLastPage) {
+      self.setData({
+        page: self.data.page + 1
+      });
+      console.log('当前页' + self.data.page);
+      this.fetchCommentData(self.data);
+    }
+    else {
+      wx.showToast({
+        title: '没有更多内容',
+        mask: false,
+        duration: 1000
+      });
+    }
+  },
+  //提交评论
+  formSubmit: function (e) { 
+    var self = this;   
+    var name = self.data.userInfo.nickName;
+    var email = "test@test.com";
+    var comment = e.detail.value.inputComment;
+    var author_url =  self.data.userInfo.avatarUrl;
+    
+    var postID = e.detail.value.inputPostID;
+    if (comment.length===0 )
+    {
+      self.setData({
+        'dialog.hidden': false,
+        'dialog.title': '提示',
+        'dialog.content': '没有填写评论内容。'
+
+      });
+
+    }
+    else
+    {
+      //检测授权
+      self.checkSettingStatu();
+      
+      if (self.data.isGetUserInfo)
+      {
+
+
+        wx.request({
+          url: Api.postComment(),
+          method: 'post',
+          data: {
+            post: postID,
+            author_name: name,
+            author_email: email,
+            content: comment,
+            author_url: author_url
+          },
+          header: {
+            'content-type': 'application/json'
+          },
+          success: function (res) {
+            //console.log(res.data)
+            if (res.statusCode == 201) {
+              self.setData({
+                'dialog.hidden': false,
+                'dialog.title': '提示',
+                'dialog.content': '评论成功',
+                content: ''
+
+              });
+
+              self.fetchCommentData(self.data);
+
+            }
+            else {
+
+              if (res.data.code == 'rest_comment_login_required') {
+                self.setData({
+                  'dialog.hidden': false,
+                  'dialog.title': '提示',
+                  'dialog.content': '需要开启在WordPress rest api 的匿名评论功能！'
+
+                });
+              }
+              else if (res.data.code == 'rest_invalid_param' && res.data.message.indexOf('author_email') > 0) {
+                self.setData({
+                  'dialog.hidden': false,
+                  'dialog.title': '提示',
+                  'dialog.content': 'email填写错误！'
+
+                });
+              }
+              else {
+                self.setData({
+                  'dialog.hidden': false,
+                  'dialog.title': '提示',
+                  'dialog.content': '评论失败,' + res.data.message
+
+                });
+              }
+
+            }
+
+          },
+          fail: function (res) {
+            //console.log(res.data) 
+          }
+        });
+
+      }
+
+    }
+   
+  },
+  // 检测授权状态
+  checkSettingStatu: function (cb) {
+    var that = this;
+    // 判断是否是第一次授权，非第一次授权且授权失败则进行提醒
+    wx.getSetting({
+      success: function success(res) {
+        console.log(res.authSetting);
+        var authSetting = res.authSetting;
+        if (util.isEmptyObject(authSetting)) {
+          console.log('首次授权');
+        } else {
+          console.log('不是第一次授权', authSetting);
+          // 没有授权的提醒
+          if (authSetting['scope.userInfo'] === false) {
+            wx.showModal({
+              title: '用户未授权',
+              content: '如需正常使用评论的功能，请授权管理中选中“用户信息”，然后点按确定后再次提交评论。',
+              showCancel: false,
+              success: function (res) {
+                if (res.confirm) {
+                  console.log('用户点击确定')
+                  wx.openSetting({
+                    success: function success(res) {
+                      console.log('openSetting success', res.authSetting);
+                      var as = res.authSetting;
+                      for (var i in as) {
+                        
+                        if(as[i])
+                        {
+                          //获取用户信息
+                          app.getUserInfo(function (userInfo)                             {
+                            //更新数据
+                            that.setData({
+                              userInfo: userInfo,
+                              isGetUserInfo: true
+                            })
+                          });
+                        }
+                        
+                      }                     
+
+                    }
+                  });
+                }
+              }
+            })
+          }
+        }
+      }
+    });
+  },
+  confirm: function () {
+    this.setData({
+      'dialog.hidden': true,
+      'dialog.title': '',
+      'dialog.content': ''
+    })
+  }
+  
+
+})
---
+++ pages/index/index.js
@@ -0,0 +1,379 @@
+
+var Api = require('../../utils/api.js');
+var util = require('../../utils/util.js');
+var WxParse = require('../../wxParse/wxParse.js');
+
+
+Page({
+  data: {
+    title: '文章列表',
+    postsList: {},
+    pagesList: {},
+    categoriesList: {},
+    postsShowSwiperList: {},
+
+
+    isLastPage:false,
+    
+    page: 1,
+    search: '',
+    categories: 0,
+
+    scrollHeight: 0,
+
+    displayHeader:"none",
+    displaySwiper: "block",
+    floatDisplay: "none",
+
+
+    //  侧滑菜单
+    maskDisplay: 'none',
+    slideHeight: 0,
+    slideRight: 0,
+    slideWidth: 0,
+    slideDisplay: 'block',
+    screenHeight: 0,
+    screenWidth: 0,
+    slideAnimation: {}
+
+
+  },
+  formSubmit: function (e) {
+    var url = '../list/list'
+    if (e.detail.value.input != '') {
+      url = url + '?search=' + e.detail.value.input;
+    }
+    wx.navigateTo({
+      url: url
+    })
+  },
+  onShareAppMessage: function () {
+    return {
+      title: '“蔡妈看看”网站微信小程序',
+      path: 'pages/index/index',
+      success: function (res) {
+        // 转发成功
+      },
+      fail: function (res) {
+        // 转发失败
+      }
+    }
+  },
+  onReachBottom: function () {
+
+    //console.log("xialajiazai");  
+   
+  },
+  onLoad: function (options) {
+    var self = this;
+    if (options.categoryID && options.categoryID != 0) {
+      self.setData({
+        categories: options.categoryID
+      })
+    }
+    if (options.search && options.search != '') {
+      self.setData({
+        search: options.search
+
+      })
+    }
+
+    this.fetchTopFivePosts();
+    
+
+    wx.getSystemInfo({
+      success: function (res) {
+        //console.info(res.windowHeight);
+        self.setData({
+          scrollHeight: res.windowHeight,
+          //screenWidth: res.windowWidth,
+          slideHeight: res.windowHeight,
+          slideRight: res.windowWidth,
+          slideWidth: res.windowWidth * 0.7
+        });
+      }
+    });
+  },
+
+  
+  fetchTopFivePosts: function () {
+    var self = this;
+    self.setData({
+      postsShowSwiperList: []
+    });
+
+
+    //先优先获取置顶的文章
+    wx.request({
+      url: Api.getStickyPosts(),
+      success: function (response) {
+        if (response.data.length > 0) {
+
+          self.setData({
+            postsShowSwiperList: self.data.postsShowSwiperList.concat(response.data.map(function (item) {
+              item.firstImage = Api.getContentFirstImage(item.content.rendered);
+              return item;
+            }))
+          });
+
+          self.fetchPostsData(self.data);
+        }
+
+        else {
+          self.setData({
+            displaySwiper: "none",
+            displayHeader:"block"
+
+          });
+
+          self.fetchPostsData(self.data);
+
+        }
+
+
+      },
+      fail: function (response) {
+        //var temp = response.data;
+
+      }
+    });
+
+
+
+
+
+  },
+
+  //获取文章列表数据
+  fetchPostsData: function (data) {
+    var self = this;
+
+    
+    if (!data) data = {};
+    if (!data.page) data.page = 1;
+    if (!data.categories) data.categories = 0;
+    if (!data.search) data.search = '';
+    if (data.page === 1) {
+      self.setData({
+        postsList: []
+      });
+    };
+
+    wx.showLoading({
+      title: '加载中',
+    })
+
+    wx.request({
+      url: Api.getPosts(data),
+      success: function (response) {
+
+        if (response.statusCode === 200) {
+
+          if (response.data.length<6)
+          {
+            self.setData({
+              isLastPage: true
+            });
+          }
+
+          //console.log(response);       
+          self.setData({
+            //postsList: response.data
+           
+            floatDisplay: "block",
+            postsList: self.data.postsList.concat(response.data.map(function (item) {
+              //var strSummary = util.removeHTML(item.content.rendered);
+              // item.summary = util.cutstr(strSummary, 200, 0);
+              var strdate = item.date
+              item.firstImage = Api.getContentFirstImage(item.content.rendered);
+              item.date = util.cutstr(strdate, 10, 1);
+              return item;
+            })),
+
+          });
+
+
+          if (data.page == 1) {
+            
+            self.fetchCategoriesData();
+          }
+
+
+          setTimeout(function () {
+            wx.hideLoading();
+            wx.showToast({
+              title: '加载完毕',
+              icon: 'success',
+              duration: 900
+            })
+          }, 900)
+         
+
+        }
+        else
+        {
+
+        
+          if (response.data.code =="rest_post_invalid_page_number")
+          {
+
+            self.setData({
+              isLastPage:true
+            });
+            wx.showToast({
+              title: '没有更多内容',
+              mask:false,
+              duration: 1500
+            });
+          }
+          else
+          {
+            wx.showToast({
+              title: response.data.message,
+              duration: 1500
+            })
+          }
+
+          
+
+        }
+
+
+
+      }
+    });
+  },
+  //底部刷新
+  loadMore: function (e) {
+    
+    var self = this;
+    if (!self.data.isLastPage)
+    {
+      self.setData({
+        page: self.data.page + 1
+      });
+      console.log('当前页' + self.data.page);
+      this.fetchPostsData(self.data);
+    }
+    else
+    {
+      wx.showToast({
+        title: '没有更多内容',
+        mask: false,
+        duration: 1000
+      });
+    }
+  },
+  //获取页面列表
+  fetchPagesData: function () {
+    var self = this;
+    wx.request({
+      url: Api.getPages(),
+      success: function (response) {
+        self.setData({
+          pagesList: response.data
+        });
+      }
+    });
+  },
+
+  //获取分类列表
+  fetchCategoriesData: function () {
+    var self = this;
+    wx.request({
+      url: Api.getCategories(),
+      success: function (response) {
+        self.setData({
+          categoriesList: response.data
+        });
+      }
+    });
+  },
+
+
+  // 跳转至查看文章详情
+  redictDetail: function (e) {
+    // console.log('查看文章');
+    var id = e.currentTarget.id,
+      url = '../detail/detail?id=' + id;
+    wx.navigateTo({
+      url: url
+    })
+  },
+
+  //跳转至某分类下的文章列表
+  redictIndex: function (e) {
+    //console.log('查看某类别下的文章');  
+    var id = e.currentTarget.dataset.id;
+    var name = e.currentTarget.dataset.item;
+    var url = '../list/list?categoryID=' + id + '&categoryName=' + name;
+    wx.navigateTo({
+      url: url
+    });
+  },
+
+  //跳转至某分类下的文章列表
+  redictHome: function (e) {
+    //console.log('查看某类别下的文章');  
+    var id = e.currentTarget.dataset.id,
+      url = '/pages/index/index';
+
+    wx.switchTab({
+      url: url
+    });
+  },
+
+
+  //浮动球移动事件
+  ballMoveEvent: function (e) {
+    var touchs = e.touches[0];
+    var pageX = touchs.pageX;
+    var pageY = touchs.pageY;
+    if (pageX < 25) return;
+    if (pageX > this.data.screenWidth - 25) return;
+    if (this.data.screenHeight - pageY <= 25) return;
+    if (pageY <= 25) return;
+    var x = this.data.screenWidth - pageX - 25;
+    var y = this.data.screenHeight - pageY - 25;
+    this.setData({
+      ballBottom: y,
+      ballRight: x
+    });
+  },
+
+  //浮动球点击 侧栏展开
+  ballClickEvent: function () {
+    slideUp.call(this);
+  },
+
+  //遮罩点击  侧栏关闭
+  slideCloseEvent: function () {
+    slideDown.call(this);
+  }
+
+})
+
+//侧栏展开
+function slideUp() {
+  var animation = wx.createAnimation({
+    duration: 600
+  });
+  this.setData({ maskDisplay: 'block' });
+  animation.translateX('100%').step();
+  this.setData({
+    slideAnimation: animation.export()
+  });
+}
+
+//侧栏关闭
+function slideDown() {
+  var animation = wx.createAnimation({
+    duration: 800
+  });
+  animation.translateX('-100%').step();
+  this.setData({
+    slideAnimation: animation.export()
+  });
+  this.setData({ maskDisplay: 'none' });
+}
+
